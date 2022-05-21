5a665e32a4ce6aa6fb665b0127fb3ecfeb85f8e9
Audi Bramastya Oroh <audibram14@gmail.com>
2022-05-21 12:06:19 +0700

    add front end and update backend

--- src/main/java/com/projects/repository/BookRepository.java
+++ src/main/java/com/projects/repository/BookRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("SELECT b FROM Book b WHERE b.name LIKE %?1%" + " OR b.isbn LIKE %?1%" + " OR b.serialName LIKE %?1%")
-    public List<Book> search(String keyword);
+    List<Book> search(String keyword);
 }
